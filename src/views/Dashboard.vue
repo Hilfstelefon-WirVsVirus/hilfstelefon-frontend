3a42f31c710d2e7c089716f46c782ca675be7f22
Markus Willems <m.willems@traumfewo.me>
2020-03-22 09:55:07 +0100

    Add dashboard and registration

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-container dashboard">
+    <h1>{{ $t('dashboard.title') }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+};
+</script>
