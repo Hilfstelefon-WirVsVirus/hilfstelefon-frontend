4a6c888fcf9ed174df81a75a84837ae51d09d2b8
Johannes Konze <konze@traum-ferienwohnungen.de>
2020-03-22 11:26:43 +0100

    Add waveform.js for audio visualisation

--- src/components/IssueCard.vue
+++ src/components/IssueCard.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="issue-card">
     <div
-      class="issue-card__local-info  location" v-bind:class="issueCardState">
-        <img class="location__icon" alt="local-icon" src="/img/icons/location-arrow.svg">
-        <span class="location__city">{{ city }} / </span>
-        <span class="location__plz">{{ zip }}</span>
+      class="issue-card__local-info  location">
+      <img class="location__icon" alt="local-icon" src="/img/icons/location-arrow.svg">
+      <span class="location__city">{{ city }} / </span>
+      <span class="location__plz">{{ zip }}</span>
     </div>
     <h3 class="issue-card__headline">{{ headline }}</h3>
     <p class="issue-card__text">{{ text }}</p>
     <div class="issue-card__record record">
-      <img class="record__waveform" alt="waveform" src="/img/icons/waveform.svg">
-      <button class="record__playbutton">
+      <div class="record__waveform" ref="waveformcontainer"></div>
+      <button @click="$wavesurfer.play()" class="record__playbutton">
         <img alt="play-icon" src="/img/icons/play.svg">
       </button>
     </div>
@@ -22,6 +22,8 @@
 </template>
 
 <script>
+import WaveSurfer from 'wavesurfer.js';
+
 export default {
   name: 'IssueCard',
   props: {
@@ -29,96 +31,94 @@ export default {
     text: String,
     city: String,
     zip: String,
+    audiofile: String,
   },
-
-  data: {
-    isOpen: true,
-    isProgress: null,
-    isClosed: null,
-    isUnassigned: null,
+  data() {
+    return {
+      $wavesurfer: null,
+    };
   },
-
-  computed: {
-    issueCardState: function () {
-      return {
-        'issue-card__local-info--open': this.isOpen,
-        'issue-card__local-info--progress': this.isProgress,
-        'issue-card__local-info--closed': this.isClosed,
-      }
-    }
-  }
+  mounted() {
+    const wavesurfer = WaveSurfer.create({
+      container: this.$refs.waveformcontainer,
+      waveColor: '#516281',
+      progressColor: '#8FCDC1',
+    });
+    this.$wavesurfer = wavesurfer;
+    wavesurfer.load('https://api.twilio.com/2010-04-01/Accounts/ACb1d198ab96bb32bc506f9ee5878839b4/Recordings/RE83d339e74e377ff8239b1eb4d2a2ec37.mp3?Download=true&_ga=2.5597747.1880444191.1584738286-826576866.1584738286');
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.issue-card {
-  width: 100%;
-  max-width: 800px;
-  margin: 50px auto;
-  padding: 30px;
-  background-color: #EDF2F7;
-  border-radius: 21px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  @include shadow(10);
-
-  &__headline {
-    font-size: 30px;
-    margin: 10px 0;
-    padding: 0;
-  }
-
-  &__text {
-    text-align: left;
-    max-width: 500px;
-    margin: 0 0 20px;
-  }
-
-  &__record {
+  .issue-card {
     width: 100%;
-  }
-
-  &__assign {
-    @extend .button;
-    padding: 10px 10px 10px 20px;
-    margin-top: 20px;
-    border-radius: 30px;
-    align-self: flex-end;
+    max-width: 800px;
+    margin: 50px auto;
+    padding: 30px;
+    background-color: #EDF2F7;
+    border-radius: 21px;
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    align-items: flex-start;
+    @include shadow(10);
 
-    &__label {
-      padding-right: 10px;
+    &__headline {
+      font-size: 30px;
+      margin: 10px 0;
+      padding: 0;
+    }
+
+    &__text {
+      text-align: left;
+      max-width: 500px;
+      margin: 0 0 20px;
+    }
+
+    &__record {
+      width: 100%;
+    }
+
+    &__assign {
+      @extend .button;
+      padding: 10px 10px 10px 20px;
+      margin-top: 20px;
+      border-radius: 30px;
+      align-self: flex-end;
+      display: flex;
+      align-items: center;
+
+      &__label {
+        padding-right: 10px;
+      }
     }
   }
-}
 
-.location {
-  &__icon {
+  .location {
+    &__icon {
       margin-right: 10px;
-  }
-}
-
-.record {
-  display: flex;
-  align-items: center;
-
-  &__waveform {
-    flex-grow: 1;
-    margin-right: 20px;
+    }
   }
 
-  &__playbutton {
-    @extend .button;
-    padding: 10px;
-    border-radius: 100%;
+  .record {
     display: flex;
-    justify-content: center;
     align-items: center;
-    flex-shrink: 0;
-    flex-grow: 0;
+
+    &__waveform {
+      flex-grow: 1;
+      margin-right: 20px;
+    }
+
+    &__playbutton {
+      @extend .button;
+      padding: 10px;
+      border-radius: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-shrink: 0;
+      flex-grow: 0;
+    }
   }
-}
 </style>
